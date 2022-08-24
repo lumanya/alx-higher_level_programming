13b3a05348b8465b7d591270afe589db26a92f07
lumanya <shaibulumanya@gmail.com>
2022-08-25 01:53:38 +0300

    check for negative number remainder

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-rem = number % 10
-if rem > 5:
-    print(f"Last digit of {number} is {rem} and is greater than 5")
-elif rem < 6 and rem != 0:
-    print(f"Last digit of {number} is {rem} and is less than 6 and not 0")
+
+if number < 0:
+    remainder = number % (-10)
 else:
-    print(f"last digit of {number} is {rem} and is 0")
+    remainder = number % 10
+
+if remainder > 5:
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, remainder))
+elif remainder == 0:
+    print("Last digit of {} is {} and is 0"
+          .format(number, remainder))
+else:
+    print("Last digi of {} is {} and is less than 6 and not 0"
+          .format(number, remainder))
