f030a95bec8dbf3aa9ca69f84b9d52983f4e16c0
lumanya <shaibulumanya@gmail.com>
2022-08-25 12:53:21 +0300

    reslove keyword erro

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv[1:])
     print("{} {}{}".format(length, "arguments" if (length) != 1 else
-                           "arguemnts", "." if (length) != 0 else ":"))
+                           "argument", "." if (length) == 0 else ":"))
     for i in range(length):
         print("{}: {}".format(i + 1, argv[i + 1]))
