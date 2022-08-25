39d3d65a546c191d1e171f64fceadc57c0aa76b5
lumanya <shaibulumanya@gmail.com>
2022-08-25 12:50:47 +0300

    edit __name__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
-import sys
-
-length = len(sys.argv)
-
-if length == 1:
-    print("0 arguments.")
-elif length == 2:
-    print("{} argument:\n1: {}".format(length - 1, sys.argv[1]))
-else:
-    print("{} arguments:".format(length - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv[1:])
+    print("{} {}{}".format(length, "arguments" if (length) != 1 else
+                           "arguemnts", "." if (length) != 0 else ":"))
+    for i in range(length):
+        print("{}: {}".format(i + 1, argv[i + 1]))
