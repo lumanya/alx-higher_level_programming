bc66c2c8f02b58d449fb99720a097326401446c0
lumanya <shaibulumanya@gmail.com>
2022-09-03 04:05:06 +0300

    print 2 d matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+new_list = []
+
+
+def square_matrix_simple(matrix=[]):
+    for i in matrix:
+        new_list.append(list(map(lambda x: x ** 2, i)))
+        return new_list
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+Python-more Data structure
