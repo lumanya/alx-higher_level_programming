15c779cd179e8f9d70aa4c6500e4721365aff1a3
lumanya <shaibulumanya@gmail.com>
2022-09-13 14:46:06 +0300

    define sqaure class with private attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-sqaure
+
+Define class Square
+"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def set_size(self, size):
+        self.__size = size
