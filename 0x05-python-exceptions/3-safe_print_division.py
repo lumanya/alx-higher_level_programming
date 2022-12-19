ea1994e11e01caee3590f8fac029bdba66100037
lumanya <shaibulumanya@gmail.com>
2022-12-19 14:43:19 +0300

    safe division of two integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    div = 0
+    try:
+        div = a / b
+    except (ZeroDivisionError, TypeError):
+        return None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
