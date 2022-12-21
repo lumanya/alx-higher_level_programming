2665498ad83b5221dcb6e3f04513a7660009ee45
lumanya <shaibulumanya@gmail.com>
2022-12-21 16:07:03 +0300

    square class with private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""
-Module 1-square
-Define class Square
-"""
+"""Square class with private attribute """
+
 
 class Square:
-    """
-    Class Square Definition
+    """Square class with  """
 
-    Args:
-        size: size of a side in square
-    """
     def __init__(self, size):
-        """
-        Initializes sqaure
-
-        Attributes:
-             size: size of a side of square
+        """Instantiate objects
+        Args:
+            size (int): size of the square
         """
         self.__size = size
