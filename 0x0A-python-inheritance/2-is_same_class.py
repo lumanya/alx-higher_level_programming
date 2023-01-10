77ec3d67201493819afa9204f0e9823c0a69447e
lumanya <shaibulumanya@gmail.com>
2023-01-10 14:02:21 +0300

    use == ioperator

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,22 +2,24 @@
 """
 Module 2-is_same_class
 
-contains function that returns True is the objects is exactly an instance
+contains method is_same_class
+returns True is the objects is exactly an instance
 of the spcefified class, otherwise False
 """
 
 
 def is_same_class(obj, a_class):
     """
-    Return True if the objects is eactyl an instance of the specified class,
-    otherwise False
-
+    Notes:
+      use type() to get specific class
+      use isinstance() to get class and any parent classes too
+      use issubclass() to get what objects is a subclass o
     Args:
        obj (instance):  instance of a class
        a_class (class): class
      """
 
-    if type(obj) is  a_class:
+    if type(obj) ==  a_class:
         return True
     else:
         return False
