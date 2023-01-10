43c393d6bf7760471e84df02af79838e1835cb5e
lumanya <shaibulumanya@gmail.com>
2023-01-10 16:41:58 +0300

    sqaure class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module 10-square
+
+contains parent class Rectangle
+with instation of private attributes  widtha and height, validated by parent
+
+contains Square class
+with instantiation of private attributes size, validated by parent
+with area method
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ inherit from Rectangle
+    method:
+     __init__(self, width, height)
+    """
+
+    def __init__(self, size):
+        """Validate and initialze widtha and height
+        Args:
+          width(int): private
+          height(int): private
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """prints {Square] <width><height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
