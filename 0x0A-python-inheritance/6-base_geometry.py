04497b3cde3a419d575075929bdb9feeb1e1d55b
lumanya <shaibulumanya@gmail.com>
2023-01-10 14:50:56 +0300

    base geometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 2-geometry
+contains public instance method area that raise Exception if called
+"""
+
+
+class BaseGeometry():
+    """
+    Method:
+     area(self): public instance raise Exception if called
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
