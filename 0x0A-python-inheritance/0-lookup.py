3e094916c9ccd62a46637687c3d8265057e5cef3
lumanya <shaibulumanya@gmail.com>
2023-01-10 12:20:52 +0300

    lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-looup
+
+contains lookup function that returns  attributs and method of an objects
+"""
+
+
+def lookup(obj):
+    """ Function that returns available attributs and methodof na objetcs """
+
+    return dir(obj)
