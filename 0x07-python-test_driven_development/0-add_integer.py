23992418a5315c82e33b514517e0583b9395ddf4
lumanya <shaibulumanya@gmail.com>
2023-02-02 13:45:26 +0300

    returned type

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,8 +11,8 @@ def add_integer(a, b=98):
         a and b must be first casted to integers if the are float
         Returns: a and b
     """
-    if  not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
