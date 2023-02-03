44e73acd790edccbcfe2e8ddc4aedaec173d8abd
lumanya <shaibulumanya@gmail.com>
2023-02-03 16:03:18 +0300

    renamed private attribute

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,12 +11,12 @@ class Base:
     __nb_obejects (private): private attribute
     """
 
-    __nb_projects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """ Initialize instance of a class"""
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+           Base.__nb_objects += 1
+           self.id = Base.__nb_objects
