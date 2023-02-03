6a6296724dae1131383475277c8b31ed7469e9dc
lumanya <shaibulumanya@gmail.com>
2023-02-03 16:04:39 +0300

    edit pep8 style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,5 +18,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
