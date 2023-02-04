c67031419b526d41d73e0a27296fddbc0d1213ca
lumanya <shaibulumanya@gmail.com>
2023-02-04 15:10:35 +0300

    edit return value

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,6 +28,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if not list_dictionaries:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
