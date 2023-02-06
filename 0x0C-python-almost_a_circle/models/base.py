eff1c89e0c946798d681f78784309bbf2a32e20d
lumanya <shaibulumanya@gmail.com>
2023-02-06 14:56:52 +0300

    implemented csv file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
  base is Module contains base class
 """
 import json
+import csv
 
 
 class Base:
@@ -16,6 +17,8 @@ class Base:
     from_json_string(json_string)
     create(cls, **dictionary)
     load_from_file(cls)
+    save_to_csv_file(cls)
+    load_from_file_csv(cls)
     """
 
     __nb_objects = 0
@@ -52,6 +55,19 @@ class Base:
                 objs.append(cls.to_dictionary(o))
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(cls.to_json_string(objs))
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        objs = []
+        filename = cls.__name__ + ".csv"
+
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for o in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([o.id, o.width, o.height, o.x, o.y])
+                if cls.__name__ == "Square":
+                    writer.writerow([o.id, o.size, o.x, o.y])
 
     @staticmethod
     def from_json_string(json_string):
@@ -91,3 +107,27 @@ class Base:
         except FileNotFoundError:
             return list_instances
         return list_instances
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ load data from csv file"""
+        objs = []
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        dic = {"id": int(row[0]), "width": int(row[1]),
+                               "height": int(row[2]), "x": int(row[3]),
+                               "y": int(row[4])}
+                    if cls.__name__ == "Square":
+                        dic = {"id": int(row[0]),
+                               "size": int(row[1]),
+                               "x": int(row[2]),
+                               "y": int(row[3])}
+                    o = cls.create(**dic)
+                    objs.append(o)
+        except FileNotFoundError:
+            return objs
+        return objs
