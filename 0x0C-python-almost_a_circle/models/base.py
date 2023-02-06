2c42fad0a35fb36c86ecfda6a4e3d5a8843d697a
lumanya <shaibulumanya@gmail.com>
2023-02-06 11:48:08 +0300

    add class method save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@ class Base:
     __nb_obejects (private): private attribute
     method:
     to_json_string(list_dictionaries)
-
+    save_to_file(cls, list_objs)
     """
 
     __nb_objects = 0
@@ -27,7 +27,25 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ static method that python dictionary to json"""
         if not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ class method that writes the JSON string represnation
+         of list_objs toa file
+        Args:
+         cls - class
+         list_objs - list of instnances who inretits of Bse
+        """
+        objs = []
+        filename = cls.__name__ + ".json"
+
+        if list_objs is not None:
+            for o in list_objs:
+                objs.append(cls.to_dictionary(o))
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(objs))
