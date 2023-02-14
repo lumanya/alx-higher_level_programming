6445d3c493e3a5a860125e5485e4bd009f12af95
lumanya <shaibulumanya@gmail.com>
2023-02-14 23:50:59 +0300

    number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- list the number of records with the smae score  in the table second_table
 
-SELECT score, COUNT(score) AS number from second_table GROUP BY score
- ORDER BY `number` DESC;
+SELECT score, COUNT(score) AS 'number' from second_table GROUP BY score
+ ORDER BY numbe DESC;
