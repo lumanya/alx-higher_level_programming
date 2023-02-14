48083bc60b307b1aef5e5ce0964531e8d741fcf5
lumanya <shaibulumanya@gmail.com>
2023-02-15 00:22:45 +0300

    convert to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Go to UTF8, convert the hbtn_0c_0 databsde to UTF8(utf8mb4, collate
+-- utf8mb4_unicode_ci
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
