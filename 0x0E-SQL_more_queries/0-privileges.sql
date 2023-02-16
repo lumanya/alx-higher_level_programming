942c00f80a49b3f140bd54aa1d924b9009e6de76
lumanya <shaibulumanya@gmail.com>
2023-02-16 15:10:39 +0300

    list privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that list all privileges of the MySQL users 'user_0d_1' and
+-- user_0d_1 on localhost
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
