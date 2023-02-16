1d44ea823e53c030f5a341c5d53aef1fcf6d2f9a
lumanya <shaibulumanya@gmail.com>
2023-02-17 01:58:04 +0300

    edited on as varible

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,9 +7,9 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 
-SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
+SELECT tv_genres.name AS genre, COUNT(*) AS 'number_of_shows'
     FROM tv_genres
 INNER JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
   	GROUP BY genre
-  	ORDER BY number_shows DESC;	
+  	ORDER BY number_of_shows DESC;
