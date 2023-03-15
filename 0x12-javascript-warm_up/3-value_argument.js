1df584fe40584962d944896488bbf427484c18f7
lumanya <shaibulumanya@gmail.com>
2023-03-15 09:00:07 +0300

    [REFACTOR] the code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,10 +3,10 @@
   script rhat print first argument passed to it
   - if no argumnets are passed to the script, print "No Argument"
 */
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)[0];
 
-if (args.length === 0) {
+if (args === undefined) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(args);
 }
