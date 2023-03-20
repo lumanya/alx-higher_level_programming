5bb3d50f1e968d83d9c0eeaab014ee79173c53f0
lumanya <shaibulumanya@gmail.com>
2023-03-20 09:54:54 +0300

    [REFACTOR] the code

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,4 @@
 
 const argv = process.argv.slice(2);
 
-if (argv.length === 0) {
-  console.log('undefined is undefined');
-} else if (argv.length === 1) {
-  console.log(argv[0] + ' is undefined');
-} else console.log(argv[0] + ' is ' + argv[1]);
+console.log(argv[0] + ' is ' + argv[1]);
