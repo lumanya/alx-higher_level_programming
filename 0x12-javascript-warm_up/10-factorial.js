3deede8d782f0c1af0d3dd1a806fe8fdb7023bfe
lumanya <shaibulumanya@gmail.com>
2023-03-20 11:41:58 +0300

    [factorail] of argumebts provided

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const n = Number(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+console.log(factorial(n));
