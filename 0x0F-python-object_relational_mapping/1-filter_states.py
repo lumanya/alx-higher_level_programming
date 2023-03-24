e51076f41b74cc46c55d23c39b612c60c8096af1
lumanya <shaibulumanya@gmail.com>
2023-03-24 17:11:01 +0300

    removed binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,8 +20,7 @@ if __name__ == "__main__":
 
     # create cursor to exec queries using SQl
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id
-                ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     query_rows = cur.fetchall()
     for row in query_rows:
