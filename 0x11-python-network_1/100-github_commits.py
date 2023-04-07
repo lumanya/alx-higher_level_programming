89351f7db7d590ebb3d2883cfb660a5a0375aa1e
lumanya <shaibulumanya@gmail.com>
2023-04-07 16:49:29 +0300

    interview quetsions

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo_name,owner)
+    with requests.get(url) as r:
+        if r.status_code == 200:
+            commits = r.json()
+            for commit in commits[:10]:
+                sha = commit['sha']
+                author_name = commit['commit']['author']['name']
+                print(f"{sha}: {author_name}")
