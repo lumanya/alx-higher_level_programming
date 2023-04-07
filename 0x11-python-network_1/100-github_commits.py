7d207d6dac87f0bdbd4eddf37f548eda73a63cda
lumanya <shaibulumanya@gmail.com>
2023-04-07 17:01:42 +0300

    remove print statement

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
     repo_name = sys.argv[1]
     owner = sys.argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name)
-    print(url)
     with requests.get(url) as r:
         if r.status_code == 200:
             commits = r.json()
             for commit in commits[:10]:
-                sha = commit['sha']
-                author_name = commit['commit']['author']['name']
+                sha = commit.get('sha')
+                author_name = commit.get('commit').get('author').get('name')
                 print(f"{sha}: {author_name}")
