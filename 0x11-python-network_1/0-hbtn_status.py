2ac2f6bf1845836b6f3fa4c7a9c4c7a24cfc9a4a
lumanya <shaibulumanya@gmail.com>
2023-04-07 13:01:24 +0300

    fix whitespace

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         html = response.read()
- 
+
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
