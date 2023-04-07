c809fcc3cd9f4a5bcf076d786b4f9a98dd2293cb
lumanya <shaibulumanya@gmail.com>
2023-04-07 12:59:19 +0300

    add dunder function

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,15 +3,15 @@
 0-hbtn_status- is the module that fetches https://alx-intranet.hbtn.io/status
 bu use urllib package
 """
-
-
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    html = html.decode('utf-8')
 
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.encode('utf-8')))
+if __name__ == '__main__':
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+ 
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
