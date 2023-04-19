248388583b007718dc9c76d35c74cdfb89db4722
lumanya <shaibulumanya@gmail.com>
2023-04-20 01:58:50 +0300

    pep8 style add white space

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306,user=argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute(
