a79539f22786d3be075a0a846d449601dcd8121b
lumanya <shaibulumanya@gmail.com>
2023-04-21 16:05:36 +0300

    add city model

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+ Script contain Class Mapping with tables
+"""
+
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
+
+class City(Base):
+    """
+    class definition of City with inherits from Base Class
+    Args:
+    id (int): unque identifier which is primary key
+    name (str): name of the city
+    state_id (int) foreignkey
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
