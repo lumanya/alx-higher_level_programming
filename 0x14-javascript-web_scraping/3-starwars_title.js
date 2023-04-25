5a5b43148a7ab78a7748d8da01435951ade762ac
lumanya <shaibulumanya@gmail.com>
2023-04-25 17:34:21 +0300

    refactor the code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-// Movie id
-const movieId = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-console.log(url);
-// Get title of the movie from Star wars api
-request(url, (error, response, body) => {
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     const data = JSON.parse(body);
     console.log(data.title);
