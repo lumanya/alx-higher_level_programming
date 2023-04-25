111c2cccbc70846b1106d17acd754f675d5b6ad2
lumanya <shaibulumanya@gmail.com>
2023-04-25 16:33:23 +0300

    specify encoding

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,8 +6,8 @@ const filePath = process.argv[2];
 // string to write to a file
 const data = process.argv[3];
 
-fs.writeFile(filePath, data, (err) => {
+fs.writeFile(filePath, data, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.error(err);
-    return;
-  }});
+  }
+});
