7313530268084ea7b148231a8ebe67daee328ee9
lumanya <shaibulumanya@gmail.com>
2023-04-25 17:27:22 +0300

    fix errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,9 +6,11 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 console.log(url);
 // Get title of the movie from Star wars api
-request(url, { json: true }, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  }
-  console.log(body.title);
+  } else {
+	  const data = JSON.parse(body)
+	  console.log(data.title)
+	  }
 });
